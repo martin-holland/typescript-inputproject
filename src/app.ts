babb109cf493db4e07de2b0bc661eed52734dd77
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-01-28 12:08:42 +0200

    input working

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
-console.log('PMApp in TS begins...');
-
-// DOM Elements selection an OOP rendering
-
-
+function autobind(
+    _:any,
+    _2: string,
+    descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this)
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+    }
+    
+    // DOM Elements selection an OOP rendering
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
     titleInputElement: HTMLInputElement;
-    description: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
 
 
@@ -20,20 +32,49 @@ class ProjectInput {
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = "user-input";
         this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
-        this.description = this.element.querySelector("#description") as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
 
         this.configure();
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value)
+        // console.log(this.titleInputElement.value)
+        // console.log(this.descriptionInputElement.value)
+        // console.log(this.peopleInputElement.value)
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people)
+            this.clearInputs();
+        }
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler)
+        // call bind here to reconfigure how this function is going to execute
+        // When it exexcutes in the future
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     };
 
     private attach(){
